cd4d32653366d98813c44fe2241f8798c1f2547e
bram <bram.schulpen@live.nl>
2018-09-25 13:15:04 +0200

    dingen in test veranderd

--- Proftaak/Test TESSA/Test Form.Designer.cs
+++ Proftaak/Test TESSA/Test Form.Designer.cs
@@ -31,7 +31,11 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.TestPic = new System.Windows.Forms.PictureBox();
             this.buttonTest = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.TestPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // TestPic
@@ -39,7 +43,7 @@
             this.TestPic.ErrorImage = ((System.Drawing.Image)(resources.GetObject("TestPic.ErrorImage")));
             this.TestPic.Image = ((System.Drawing.Image)(resources.GetObject("TestPic.Image")));
             this.TestPic.InitialImage = ((System.Drawing.Image)(resources.GetObject("TestPic.InitialImage")));
-            this.TestPic.Location = new System.Drawing.Point(652, 56);
+            this.TestPic.Location = new System.Drawing.Point(121, 42);
             this.TestPic.Name = "TestPic";
             this.TestPic.Size = new System.Drawing.Size(85, 77);
             this.TestPic.TabIndex = 0;
@@ -56,16 +60,40 @@
             this.buttonTest.UseVisualStyleBackColor = true;
             this.buttonTest.Click += new System.EventHandler(this.buttonTest_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(482, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(85, 77);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_2);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Test_TESSA.Properties.Resources.geel;
+            this.pictureBox2.Location = new System.Drawing.Point(269, 42);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(167, 157);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.buttonTest);
             this.Controls.Add(this.TestPic);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.TestPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -74,6 +102,8 @@
 
         private System.Windows.Forms.PictureBox TestPic;
         private System.Windows.Forms.Button buttonTest;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
